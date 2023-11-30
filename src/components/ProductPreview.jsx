3024bdb712605ac794e4d19a91ac35e287801975
chris <christianmabeladi@gmail.com>
2023-11-30 12:48:44 +0100

    configuration des routes et affichage des produits

---
+++ src/components/ProductPreview.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function ProductPreview(){
+    return(
+        <h1>Product Preview</h1>
+    )
+}
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,6 +1,17 @@
+import { Outlet } from 'react-router-dom'
+
+        
+ 
+        
+
+ 
 
 export default function Products() {
     return (
-        <h1>Products</h1>
+        <>
+            <h1>Products</h1>
+            <Outlet />
+        </>
+       
     )
 }
