19de6bf36794ef58280b525f2e7f2a8b3857efbd
chris <christianmabeladi@gmail.com>
2023-12-01 10:37:23 +0100

    create component button and add a login features

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom";
+import Button from "../components/button";
+
 export default function Login() {
     return (
-        <h1>Login</h1>
+        <>
+            <h1>Login</h1>
+            <Button  linkValue='/home' buttonValue='Connexion'/>
+        </>
     )
 }
